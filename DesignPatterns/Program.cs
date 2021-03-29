650dbf1273ff3fedc8d7bd3ccdbf0d75d4a5e636
Chiranjev Koul <chiranjev.koul@cimpress.com>
2021-03-29 22:35:56 +0530

    added single responsibility principle

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace DesignPatterns
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var j = new Journal();
+    //        j.AddEntry("I cried today");
+    //        j.AddEntry("I ate a bug");
+    //        Console.WriteLine(j);
+
+    //        var p = new Persistance();
+    //        var filename = @"c:\work\journal.txt";
+    //        p.SaveToFile(j, filename, true);
+    //        Process.Start("notepad.exe",filename);
+
+    //    }
+    //}
 }
---
+++ DesignPatterns/SingleResponsibility.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace DesignPatterns
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+    }
+
+    public class Persistance
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, j.ToString());
+            }
+        }
+    }
+
+
+    class SingleResponsibility
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today");
+            j.AddEntry("I ate a bug");
+            Console.WriteLine(j);
+
+            var p = new Persistance();
+            var filename = @"c:\work\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start("notepad.exe", filename);
+
+        }
+    }
+}
